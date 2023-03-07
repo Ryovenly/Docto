1db0232c96db2ba9e6e58825079e7e5b391ffece
Akane <soranyuxe@gmail.com>
2023-03-07 16:14:11 +0100

    authentification et firestore

--- DoctoClient/src/firebase.js
+++ DoctoClient/src/firebase.js
@@ -1,18 +1,44 @@
-import * as firebase from 'firebase';
-import 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+
+import { getAnalytics } from "firebase/analytics";
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+  } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  //databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID
 }
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app)
+const auth = getAuth();
 
-firebase.initializeApp(firebaseConfig);
-firebase.firestore();
+const analytics = getAnalytics(app);
 
-export default firebase;
+export const signUp = async (email, password) => {
+  try {
+      const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+      );
+      const user = userCredential.user;
+      await addDoc(collection(db, "users"), {
+          uid: user.uid,
+          email: user.email,
+      });
+      return true
+  } catch (error) {
+      return { error: error.message }
+  }
+};
--- DoctoClient/src/handles/handlesubmit.js
+++ DoctoClient/src/handles/handlesubmit.js
@@ -1,18 +1,73 @@
 import { addDoc, collection } from "@firebase/firestore"
-import { firestore } from "../firebase_setup/firebase"
- 
+import { firestore } from "../firebase"
+
 const handleSubmit = (testdata) => {
-    const ref = collection(firestore, "test_data") // Firebase creates this automatically
- 
-    let data = {
-        testData: testdata
-    }
-    
-    try {
-        addDoc(ref, data)
-    } catch(err) {
-        console.log(err)
-    }
+    const ref = collection(firestore, "test_data") // Firebase creates this automatically
+
+    let data = {
+        testData: testdata
+    }
+
+    try {
+        addDoc(ref, data)
+        console.log("ça marche")
+    } catch (err) {
+        console.log(err)
+    }
 }
- 
+
+
+
+const signIn = async (email, password) => {
+    try {
+        const userCredential = await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        const user = userCredential.user;
+        return true
+    } catch (error) {
+        return { error: error.message }
+    }
+};
+
+const signOut = async () => {
+    try {
+        await signOut(auth)
+        return true
+    } catch (error) {
+        return false
+    }
+};
+
+const SignOut = () => {
+    const [signOut, loading, error] = useSignOut(auth);
+  
+    if (error) {
+      return (
+        <div>
+          <p>Error: {error.message}</p>
+        </div>
+      );
+    }
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+    return (
+      <div className="App">
+        <button
+          onClick={async () => {
+            const success = await signOut();
+            if (success) {
+              alert('You are sign out');
+            }
+          }}
+        >
+          Sign out
+        </button>
+      </div>
+    );
+  };
+
 export default handleSubmit
